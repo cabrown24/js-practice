94dbf4fdc654e52d94d12808614233a514b258af
cabrown24 <calebbrown1231@gmail.com>
2025-01-19 20:18:57 -0600

    Added more object practice

--- 13-object-review/script.js
+++ 13-object-review/script.js
@@ -38,4 +38,15 @@ console.log(header2.textContent);
 
 header1.innerText = "[" + bigArray + "]";  
 header2.innerText = filtMapReduced;
-header2.style.color = '#0008ff'; 
+header2.style.color = '#0008ff';
+
+let user = {
+  name: "John",
+  surname: "Smith"
+};
+user.age = 25; // add one more
+
+// non-integer properties are listed in the creation order
+for (let prop in user) {
+  alert( prop ); // name, surname, age
+}
